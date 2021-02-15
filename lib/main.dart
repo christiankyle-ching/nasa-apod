9acf4037ac2a896afe4291b77c9b06505bda7443
Christian Kyle <57316283+christiankyle-ching@users.noreply.github.com>
2021-02-15 19:25:02 +0800

    Configured AdMob Package

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_admob/firebase_admob.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
